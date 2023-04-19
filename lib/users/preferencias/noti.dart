d104b2e7effbd6f51541c431106ffc628bfa7ae3
darkjockeer <daniel.godoy.ca@gmail.com>
2023-04-19 16:47:36 -0400

    Revert "Revert "Agregar proyecto movile""
    This reverts commit 88ce7ba8b5a9febd9d0721537e00c39f05600845.

---
+++ lib/users/preferencias/noti.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class Noti{
+  static Future initialize(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async{
+    var androidInitialize = new AndroidInitializationSettings('mimap/ic_launcher');
+    
+  }
+}
